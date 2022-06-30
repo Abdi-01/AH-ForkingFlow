9da4437c44dc66d1c52a9c749d3b2defd2d37a18
anindyapramudita <anindyapramudita2111@gmail.com>
2022-06-30 19:52:08 +0700

    FR-3-Anindya-001: Create Card

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CardComponent from "../Component/Card";
+
+const LandingPage = () => {
+    return <div>
+        <CardComponent />
+    </div>
+}
+
+export default LandingPage;
