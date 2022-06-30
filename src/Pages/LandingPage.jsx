b4f3302b5c8cae920e38f2c06b6c475962b4f09c
Jackfrost24a <100822818+Jackfrost24a@users.noreply.github.com>
2022-06-30 20:01:24 +0700

    FR2-Randy-001

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import Carousel from "../Components/Carousel";
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
+}
+
+export default LandingPage;
